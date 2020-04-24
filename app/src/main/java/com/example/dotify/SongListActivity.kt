dab1be2d1bab46c517cbaa88e63f1e6284a2d094
jhoupps <j.c.houp7575@gmail.com>
2020-04-23 20:02:52 -0700

    setup for hw2, not broken this time

---
+++ app/src/main/java/com/example/dotify/SongListActivity.kt
@@ -0,0 +1,11 @@
+package com.example.dotify
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SongListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
