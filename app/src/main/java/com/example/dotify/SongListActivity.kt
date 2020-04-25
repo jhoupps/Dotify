248d9aabd0cd16430fb81c5a8abef9667f37a787
jhoupps <j.c.houp7575@gmail.com>
2020-04-25 15:04:55 -0700

    making a recyclerview attempt

--- app/src/main/java/com/example/dotify/SongListActivity.kt
+++ app/src/main/java/com/example/dotify/SongListActivity.kt
@@ -2,10 +2,15 @@ package com.example.dotify
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_songlist.*
+
+
 
 class SongListActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_songlist)
     }
 }
