6cb044c8fc5d7e92b91370be3f87f11e9f1b31d1
jhoupps <j.c.houp7575@gmail.com>
2020-04-26 16:17:11 -0700

    successfully updated song name for first song

--- app/src/main/java/com/example/dotify/SongListActivity.kt
+++ app/src/main/java/com/example/dotify/SongListActivity.kt
@@ -3,6 +3,7 @@ package com.example.dotify
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import com.ericchee.songdataprovider.Song
 import com.ericchee.songdataprovider.SongDataProvider
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_songlist.*
@@ -10,12 +11,14 @@ import kotlinx.android.synthetic.main.activity_songlist.*
 
 
 class SongListActivity : AppCompatActivity() {
+    lateinit var globalMenu: Menu
+    var listOfSongs = SongDataProvider.getAllSongs()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_songlist)
 
-        val listOfSongs = SongDataProvider.getAllSongs()
+     //   val listOfSongs = SongDataProvider.getAllSongs()
 
         val thesongAdapter = SongListAdapter(listOfSongs)
 
@@ -23,13 +26,29 @@ class SongListActivity : AppCompatActivity() {
 
         setSupportActionBar(findViewById(R.id.bar))
 
+
+        //first song
+        // globalChosenSong = listOfSongs[0]
+        
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         val inflater = menuInflater
         inflater.inflate(R.menu.bottomappbar_menu, menu)
+
+        globalMenu = menu
+        updateMenu(listOfSongs[0])
         return true
     }
 
+    private fun updateMenu(chosenSong: Song): Boolean {
+        var songTitle = chosenSong.title
+        var songArtist = chosenSong.artist
+        globalMenu.findItem(R.id.tvNowPlaying).setTitle("$songTitle - $songArtist")
+
+        return true
+
+    }
 
 }
